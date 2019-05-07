6a8a80e69478f072d5a6f17d3d5f28d9bbe0a6bd
Adrian Silva <adrian.silva@quobis.com>
2019-05-07 11:46:10 +0200

    Initial commit

---
+++ StringCalculator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  StringCalculator
+//
+//  Created by Adrián Silva on 07/05/2019.
+//  Copyright © 2019 adsilva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
